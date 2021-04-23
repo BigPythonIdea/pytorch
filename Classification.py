6c84b85ece7b0804c9fb6e87062464034f9338f7
Mooncat <48311280+klas9802@users.noreply.github.com>
2021-04-23 17:50:29 +0800

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -1,1 +1,53 @@
 
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+
+
+
+x =  torch.from_numpy(load_breast_cancer().data)    # shape (569,30)
+y =  torch.from_numpy(load_breast_cancer().target)  # shape(569)
+
+
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_hidden, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
+        self.out = torch.nn.Linear(n_hidden, n_output)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.out(x)
+        return x
+
+
+net = Net(n_feature=569, n_hidden=30, n_output=2)     # define the network
+print(net)  # net architecture
+
+optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+loss_func = torch.nn.CrossEntropyLoss()  # the target label is NOT an one-hotted
+
+plt.ion()   # something about plotting
+
+for t in range(100):
+    out = net(x)                 # input x and predict based on x
+    loss = loss_func(out, y)     # must be (1. nn output, 2. target), the target label is NOT one-hotted
+
+    optimizer.zero_grad()   # clear gradients for next train
+    loss.backward()         # backpropagation, compute gradients
+    optimizer.step()        # apply gradients
+
+    if t % 2 == 0:
+        # plot and show learning process
+        plt.cla()
+        prediction = torch.max(out, 1)[1]
+        pred_y = prediction.data.numpy()
+        target_y = y.data.numpy()
+        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        accuracy = float((pred_y == target_y).astype(int).sum()) / float(target_y.size)
+        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color':  'red'})
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
